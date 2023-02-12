8f647b0c83596fb9c509bb816284a3070a5abda7
Cletus Kingdom <cletuskingdom@gmail.com>
2023-02-12 01:22:28 +0100

    update

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Image;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class QrCodeController extends Controller
 {
     public function index($id)
     {
-        return QrCode::size(400)->BTC($id);
+        // $img = QrCode::size(400)->BTC($id);
+        // return Image::make($img)->response();
+        // return response($img)->header('Content-type', 'image/png');
         return view('qrcode', [
             'code' => $id,
         ]);
